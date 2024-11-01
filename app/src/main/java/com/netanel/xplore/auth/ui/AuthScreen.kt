4be177bb8fd9247b2f8efc4c918d8d5d62fb798a
Netanel Amar <NetanelCA2@gmail.com>
2024-11-01 18:45:31 +0200

    Added ViewModel and di for firebase auth

--- app/src/main/java/com/netanel/xplore/auth/ui/AuthScreen.kt
+++ app/src/main/java/com/netanel/xplore/auth/ui/AuthScreen.kt
@@ -1,6 +1,5 @@
 package com.netanel.xplore.auth.ui
 
-import android.content.Context
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,14 +9,9 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
-import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -25,24 +19,14 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.google.firebase.FirebaseException
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.PhoneAuthCredential
-import com.google.firebase.auth.PhoneAuthOptions
-import com.google.firebase.auth.PhoneAuthProvider
 import com.netanel.xplore.R
-import com.netanel.xplore.utils.Logger
-import kotlinx.coroutines.launch
-import java.util.concurrent.TimeUnit
 
 @Composable
-fun AuthScreen(auth: FirebaseAuth, viewModel: AuthViewModel = hiltViewModel()) {
-    var phoneNumber by remember { mutableStateOf("") }
-    var verificationCode by remember { mutableStateOf("") }
-    var verificationInProgress by remember { mutableStateOf(false) }
-    var verificationId by remember { mutableStateOf<String?>(null) }
-    val snackbarHostState = remember { SnackbarHostState() }
-    val coroutineScope = rememberCoroutineScope()
+fun AuthScreen(viewModel: AuthViewModel = hiltViewModel()) {
+    val phoneNumber by viewModel.phoneNumber
+    val verificationCode by viewModel.verificationCode
+    val verificationInProgress by viewModel.verificationInProgress
+    val snackbarHostState = viewModel.snackbarHostState
     val context = LocalContext.current
 
     Scaffold(
@@ -65,49 +49,28 @@ fun AuthScreen(auth: FirebaseAuth, viewModel: AuthViewModel = hiltViewModel()) {
             if (!verificationInProgress) {
                 OutlinedTextField(
                     value = phoneNumber,
-                    onValueChange = { phoneNumber = it },
+                    onValueChange = { viewModel.phoneNumber.value = it },
                     label = { Text(stringResource(R.string.what_is_your_number)) },
                     modifier = Modifier
                         .fillMaxWidth()
                         .padding(bottom = 8.dp)
                 )
                 Button(onClick = {
-                    verificationInProgress = true
-                    startPhoneNumberVerification(context, auth,
-                        context.getString(R.string.israel_country_code).plus(phoneNumber),
-                        onVerificationComplete = {
-                            verificationId = it
-                        },
-                        onVerificationFailed = { errorMessage ->
-                            verificationInProgress = false
-                            coroutineScope.launch {
-                                snackbarHostState.showSnackbar(errorMessage)
-                            }
-                        }
-                    )
+                    viewModel.startPhoneNumberVerification(context)
                 }) {
                     Text(text = stringResource(R.string.next_step))
                 }
             } else {
                 OutlinedTextField(
                     value = verificationCode,
-                    onValueChange = { verificationCode = it },
+                    onValueChange = { viewModel.verificationCode.value = it },
                     label = { Text(stringResource(R.string.write_this_code_to_start)) },
                     modifier = Modifier
                         .fillMaxWidth()
                         .padding(bottom = 8.dp)
                 )
                 Button(onClick = {
-                    verificationId?.let {
-                        val credential = PhoneAuthProvider.getCredential(it, verificationCode)
-                        signInWithPhoneAuthCredential(context, auth, credential,
-                            onSignInFailed = { errorMessage ->
-                                coroutineScope.launch {
-                                    snackbarHostState.showSnackbar(errorMessage)
-                                }
-                            }
-                        )
-                    }
+                    viewModel.onSignInButtonClicked(context)
                 }) {
                     Text(text = stringResource(R.string.shall_we))
                 }
@@ -116,51 +79,3 @@ fun AuthScreen(auth: FirebaseAuth, viewModel: AuthViewModel = hiltViewModel()) {
     }
 }
 
-fun startPhoneNumberVerification(
-    context: Context,
-    auth: FirebaseAuth,
-    phoneNumber: String,
-    onVerificationComplete: (String) -> Unit,
-    onVerificationFailed: (String) -> Unit
-) {
-    val options = PhoneAuthOptions.newBuilder(auth)
-        .setPhoneNumber(phoneNumber)
-        .setTimeout(60L, TimeUnit.SECONDS)
-        .setCallbacks(object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
-            override fun onVerificationCompleted(credential: PhoneAuthCredential) {
-                Logger.info("PhoneAuth", "Verification completed.")
-            }
-
-            override fun onVerificationFailed(e: FirebaseException) {
-                Logger.info("PhoneAuth", "Verification failed.\n$e")
-                onVerificationFailed(context.getString(R.string.phone_num_incorrect))
-            }
-
-            override fun onCodeSent(
-                verificationId: String,
-                token: PhoneAuthProvider.ForceResendingToken
-            ) {;
-                Logger.info("PhoneAuth", "Code sent: $verificationId")
-                onVerificationComplete(verificationId)
-            }
-        })
-        .build()
-    PhoneAuthProvider.verifyPhoneNumber(options)
-}
-
-private fun signInWithPhoneAuthCredential(
-    context: Context,
-    auth: FirebaseAuth,
-    credential: PhoneAuthCredential,
-    onSignInFailed: (String) -> Unit
-) {
-    auth.signInWithCredential(credential)
-        .addOnCompleteListener { task ->
-            if (task.isSuccessful) {
-                Logger.info("PhoneAuth", "Sign-in successful.")
-            } else {
-                Logger.info("PhoneAuth", "Sign-in failed.\n${task.exception}")
-                onSignInFailed(context.getString(R.string.otp_incorrect))
-            }
-        }
-}
--- app/src/main/java/com/netanel/xplore/auth/ui/AuthViewModel.kt
+++ app/src/main/java/com/netanel/xplore/auth/ui/AuthViewModel.kt
@@ -1,16 +1,95 @@
 package com.netanel.xplore.auth.ui
 
+import android.content.Context
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.firebase.FirebaseException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthOptions
+import com.google.firebase.auth.PhoneAuthProvider
+import com.netanel.xplore.R
+import com.netanel.xplore.utils.Logger
 import dagger.hilt.android.lifecycle.HiltViewModel
-
+import kotlinx.coroutines.launch
+import java.util.concurrent.TimeUnit
+import javax.inject.Inject
 
 /**
  * Created by netanelamar on 01/11/2024.
  * NetanelCA2@gmail.com
  */
 
-//@HiltViewModel
-class AuthViewModel: ViewModel() {
+@HiltViewModel
+class AuthViewModel @Inject constructor(
+    private val auth: FirebaseAuth
+) : ViewModel() {
 
+    var phoneNumber = mutableStateOf("")
+    var verificationCode = mutableStateOf("")
+    var verificationInProgress = mutableStateOf(false)
+    var verificationId = mutableStateOf<String?>(null)
+    val snackbarHostState = SnackbarHostState()
 
+    fun startPhoneNumberVerification(context: Context) {
+        verificationInProgress.value = true
+        val options = PhoneAuthOptions.newBuilder(auth)
+            .setPhoneNumber(context.getString(R.string.israel_country_code).plus(phoneNumber.value))
+            .setTimeout(60L, TimeUnit.SECONDS)
+            .setCallbacks(object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                override fun onVerificationCompleted(credential: PhoneAuthCredential) {
+                    Logger.info("PhoneAuth", "Verification completed.")
+                }
+
+                override fun onVerificationFailed(e: FirebaseException) {
+                    Logger.info("PhoneAuth", "Verification failed.\n$e")
+                    verificationInProgress.value = false
+                    viewModelScope.launch {
+                        snackbarHostState.showSnackbar(context.getString(R.string.phone_num_incorrect))
+                    }
+                }
+
+                override fun onCodeSent(
+                    verificationId: String,
+                    token: PhoneAuthProvider.ForceResendingToken
+                ) {
+                    Logger.info("PhoneAuth", "Code sent: $verificationId")
+                    this@AuthViewModel.verificationId.value = verificationId
+                }
+            })
+            .build()
+        PhoneAuthProvider.verifyPhoneNumber(options)
+    }
+
+    fun onSignInButtonClicked(context: Context) {
+        if (verificationCode.value.isNotBlank()) {
+            signInWithPhoneAuthCredential(context)
+        } else {
+            viewModelScope.launch {
+                snackbarHostState.showSnackbar(context.getString(R.string.otp_incorrect))
+            }
+        }
+    }
+
+    fun signInWithPhoneAuthCredential(context: Context) {
+        val credential = verificationId.value?.let {
+            PhoneAuthProvider.getCredential(it, verificationCode.value)
+        }
+
+        credential?.let {
+            auth.signInWithCredential(it)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Logger.info("PhoneAuth", "Sign-in successful.")
+                    } else {
+                        Logger.info("PhoneAuth", "Sign-in failed.\n${task.exception}")
+                        viewModelScope.launch {
+                            snackbarHostState.showSnackbar(context.getString(R.string.otp_incorrect))
+                        }
+                    }
+                }
+        }
+    }
 }
