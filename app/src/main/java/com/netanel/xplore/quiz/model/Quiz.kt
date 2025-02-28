101779c651483cade54689683b6de9be74f674a5
Netanel Amar <NetanelCA2@gmail.com>
2025-03-01 00:02:38 +0200

    Update Quiz.kt

--- app/src/main/java/com/netanel/xplore/quiz/model/Quiz.kt
+++ app/src/main/java/com/netanel/xplore/quiz/model/Quiz.kt
@@ -10,13 +10,7 @@ data class Quiz(
     //New
     val answerLockTimer: Int,
     val title: String? = null,
-    /*val description: String? = null,
-    val difficulty: DifficultyLevel? = null,*/
     val creatorId: String? = null,
-    /*val startTime: String? = null,
-    val endTime: String? = null,
-    val maxParticipants: Int? = null,
-    val tags: List<String>? = null,*/
     val isActive: Boolean = true,
     var totalScore: Int = 0
 )
